53fd7a6c219fd72a27b67ff2e6c8cd9cc0a1f33a
Shaswata Chowdhury <140137696+shaswatacp@users.noreply.github.com>
2023-10-28 23:06:08 +0600

    Add files via upload

---
+++ example1.c
@@ -0,0 +1,14 @@
+//A c program to demonstrate the use of nested for loop.
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    for(a=1;a<5;a++)
+    {
+        for(b=1;b<5;b++)
+        {
+            printf("%d",b);
+        }
+        return 0;
+    }
+}
